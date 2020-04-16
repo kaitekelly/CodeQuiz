03dc591e563bf2c1f86368bf71e38c734f4622f0
Kaite Kelly <murphy.kaite@gmail.com(opens in new tab)>
2020-04-16 13:39:53 -0700

    added styles to CSS page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let timer = document.querySelector("timer");
-let startButton = document.querySelector("btn btn-primary");
-let quizStart = document.querySelector("quiz-start");
+let timer = document.querySelector(".timer");
+let startButton = document.querySelector("#start-button");
+let quizStart = document.querySelector(".quiz-start");
 
 let quizQuestions = [ {
     question: "Which language is not one of the three core languages of the web?",
@@ -9,7 +9,7 @@ let quizQuestions = [ {
 },
 {
     question:"JavaScript variables are written in which type of case?",
-    choices: ["lowercase", "camelCase", "UPPERCASE", "CapitalCase" ]
+    choices: ["lowercase", "camelCase", "UPPERCASE", "CapitalCase" ],
     answer: "camelCase",
 },
 {
@@ -30,14 +30,59 @@ let quizQuestions = [ {
 ]
 
 let currentQuestion = 0;
-
-
 //WHEN I click the start button
+// to check your answer, place in i or 
+quizQuestions[0].answer
 
-document.getElementById("start-button").addEventListener("click", function(){
-    // add function to start quiz here
+
+
+
+
+let timerEl = querySelector("timer");
+let countdownEl = querySelector("#countdown");
+let mainEl = querySelector("main");
+let timeLeft = 
+
+startButton.addEventListener("click", function(){
+    let timerBegin = 75;
+
+    let timeInterval = setInterval(function() {
+        countdownEl.textContent = timeLeft;
+        timeLeft--;  
+        if ( //countdownEl.textContent = timeLeft;
+        timeLeft-10;
+
+        if (timeLeft < 0) {
+            // timer stops and user is taken to the highscores page
+        }
+    })
+
+    render()
+})
+
+function render() {
+    let questionData = quizQuestions[currentQuestion];
+    $("#question").text(questionData.question);
+    $("#choices").empty();
+
+    for (let i = 0; i < questionData.choices; i++) {
+        $("#options").append($("<li>").text(questionData.choices[i]));
+    }
+    currentQuestion +- 1;
+    if (currentQuestion === quizQuestions.length) {
+        currentQuestion = 0;
+    }
 }
 
+$("#submit").click(render)
+
+
+
+//Questions for loop
+// for (i = 0; i < quizQuestions.length; i++ ) {
+//     let response = 
+// }
+// add function to start quiz here
 
 // THEN a timer starts and I am presented with a question
 
@@ -65,84 +110,84 @@ document.getElementById("start-button").addEventListener("click", function(){
 
 
 //THEN I can save my initials and score - Start of Highscore List to add to
-let highscoreInput = document.querySelector("highscore-text");
-let highscoreForm = document.querySelector("highscore-form");
-let highscoreList = document.querySelector("highscore-list");
-let highscoreCountSpan = document.querySelector("highscore-count");
+// let highscoreInput = document.querySelector("highscore-text");
+// let highscoreForm = document.querySelector("highscore-form");
+// let highscoreList = document.querySelector("highscore-list");
+// let highscoreCountSpan = document.querySelector("highscore-count");
 
-let addName = [];
+// let addName = [];
 
-renderHighscores();
+// renderHighscores();
 
-function renderHighscores() {
-    // Clear highscoreList element and update highscoreCountSpan
-    highscoreList.innerHTML = "";
-    highscoreCountSpan.textContent = addName.length;
+// function renderHighscores() {
+//     // Clear highscoreList element and update highscoreCountSpan
+//     highscoreList.innerHTML = "";
+//     highscoreCountSpan.textContent = addName.length;
 
-    // Render a new li for each name added
-    for (i = 0; i < addName.length; i++) {
-        let name = addName[i];
+//     // Render a new li for each name added
+//     for (i = 0; i < addName.length; i++) {
+//         let name = addName[i];
 
-        let li = document.createElement("li");
-        li.textContent = name;
-        highscoreList.appendChild(li);
-    }
-}
+//         let li = document.createElement("li");
+//         li.textContent = name;
+//         highscoreList.appendChild(li);
+//     }
+// }
 
-//When the name is submitted to the list
-highscoreForm.addEventListener("submit", function(event) {
-    event.preventDefault();
+// //When the name is submitted to the list
+// highscoreForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
 
-    let highscoreText = highscoreInput.value.trim();
+//     let highscoreText = highscoreInput.value.trim();
 
-    if (highscoreText === "") {
-        return;
-    }
+//     if (highscoreText === "") {
+//         return;
+//     }
 
-    //Add new highscoreText to addName array, clear the input
-    addName.push(highscoreText);
-    highscoreInput.value = "";
+//     //Add new highscoreText to addName array, clear the input
+//     addName.push(highscoreText);
+//     highscoreInput.value = "";
 
-    //Re-render the highscore list
-    storeNames();
-    renderHighscores();
+//     //Re-render the highscore list
+//     storeNames();
+//     renderHighscores();
 
-});
+// });
 
-//local storage code starts here - get stored names from localStorage
-function init() {
-    //Parsing the string to the object
-    let storedNames = JSON.parse(localStorage.getItem("addName"));
+// //local storage code starts here - get stored names from localStorage
+// function init() {
+//     //Parsing the string to the object
+//     let storedNames = JSON.parse(localStorage.getItem("addName"));
 
-    // if names are retrieved from localStorage, update the addNames array
-    if (storedNames !== null) {
-        addName = storedNames;
-    }
+//     // if names are retrieved from localStorage, update the addNames array
+//     if (storedNames !== null) {
+//         addName = storedNames;
+//     }
 
-    //Render addName to DOM
-    renderAddname();
-}
+//     //Render addName to DOM
+//     renderAddname();
+// }
 
-function storeNames() {
-    //Stringify and set "addName" key to localStorage to todos array
-    localStorage.setItem("addName", JSON.stringify(addName));
-}
+// function storeNames() {
+//     //Stringify and set "addName" key to localStorage to todos array
+//     localStorage.setItem("addName", JSON.stringify(addName));
+// }
 
 //When the form is submitted
-highscoreForm.addEventListener("submit", function(event) {
-    event.preventDefault();
+// highscoreForm.addEventListener("submit", function(event) {
+//     event.preventDefault();
 
-    let highscoreText = highscoreInput.value.trim();
+//     let highscoreText = highscoreInput.value.trim();
 
-    if (highscoreText === "") {
-        return;
-}
+//     if (highscoreText === "") {
+//         return;
+// }
 
-    //Add new highscoreText to addName array, clear the input
-    addName.push(highscoreText);
-    highscoreInput.value = "";
+//     //Add new highscoreText to addName array, clear the input
+//     addName.push(highscoreText);
+//     highscoreInput.value = "";
 
-    //Store updated addName in localStorage, re-render list
-    storeAddname();
-    renderAddname();
-}); 
+//     //Store updated addName in localStorage, re-render list
+//     storeAddname();
+//     renderAddname();
+// }); 
