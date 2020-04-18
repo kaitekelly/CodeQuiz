76af39703cc8a98e8a2961f7b0b478bb2da75bc6
Kaite Kelly <murphy.kaite@gmail.com(opens in new tab)>
2020-04-17 19:39:16 -0700

    added countdown timer to javascript

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let questionNumber = document.querySelector("p");
 let rules = document.querySelector("#rules");
 let correctMessage = document.querySelector("#correct-message");
 let questionChoices = document.querySelector(".choices")
+let timeLeft = 0;
+let currentQuestion = 0;
 
 let quizQuestions = [{
         question: "Which language is not one of the three core languages of the web?",
@@ -35,13 +37,13 @@ let quizQuestions = [{
 console.log(quizQuestions);
 
 //WHEN I click the start button
-let currentQuestion = 0;
-let timerBegin = 75;
+
+
 let userChoice = 0;
-let timerEl = querySelector("#timer");
-let countdownEl = querySelector("#countdown");
-let mainEl = querySelector("main");
-let timeLeft = ("");
+let timerEl = document.querySelector("#timer");
+let countdownEl = document.querySelector("#countdown");
+let mainEl = document.querySelector("main");
+
 //Highscore variables start here
 let highscoreInput = document.querySelector("highscore-text");
 let highscoreForm = document.querySelector("highscore-form");
@@ -50,102 +52,95 @@ let highscoreCountSpan = document.querySelector("highscore-count");
 let addName = [];
 
 // Event listener for start button - quizQuestions
-//check if start-button needs a pound sign in front of it
-document.getElementById("start-button").addEventListener("click", function() {
-    beginQuiz;
-    // timerEl;
+
+document.getElementById("start-button").addEventListener("click", function () {
     startButton.style.visibility = "hidden";
     rules.style.visibility = "hidden";
-})
-console.log(startButton)
-
-// QUiz start function starts here 
-function beginQuiz(){
-    // Loop over every question
-    for (let i = 0; i < quizQuestions.length; i++) {
-        let questionNumber = questionNumber(quizQuestions[i].question);
-    }
-console.log(beginQuiz)
-    // start if statements to check if answers are correct 
-    if (userChoice === quizQuestions[0].answer) {
-        correctMessage = prompt("Correct!");
-    } else {
-        userChoice !== quizQuestion[0].answer() 
-        textDisplay = "Wrong! You lose 10 seconds!"
+    beginQuiz();
     
+})
+// Quiz start function starts here 
+//WHEN I answer a question
+//THEN I am presented with another question
+
+function beginQuiz() {
+    // Loop over every question
+    // for (let i = 0; i < quizQuestions.length; i++) {
+    //     // Is this statement below written correctly?
+    //     let questionNumber = questionNumber(quizQuestions[i].question);
+    // }
+    timeLeft = 75;
+    let timerInterval = setInterval(function () {
+        countdownEl.textContent = timeLeft;
+        timeLeft--;
+        (countdownEl.textContent = timeLeft);
+        console.log(timeLeft);
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            alert("Time is up!")
+        }
+        // timeLeft--10);
+    }, 1000);
+
+    // start if statements to check if answers are correct 
+    // if (userChoice === quizQuestions[0].answer) {
+    //     correctMessage = prompt("Correct!");
+    //     //WHEN I answer a question incorrectly
+    // } else { // not sure how to write else code
+    //     userChoice !== quizQuestion[0].answer()
+    //     textDisplay = "Wrong! You lose 10 seconds!"
+
+    // }
+}
+
+//Code to put answers on to buttons  - this code doesn't work...
+
+
+function buttonName() {
+    let choicesBtn = ["btn1", "btn2", "btn3", "btn4"];
+    for (let i = 0; i < choicesBtn.length; i++) {
+        document.getElementById(choicesBtn[i]).innerText += "<button>" + choicesBtn[i] + "</button>";
     }
 }
 
-startButton.addEventListener("click", function(){
-    let timerBegin = 75;
 
-    let timeInterval = setInterval(function() {
-        countdownEl.textContent = timeLeft;
-        timeLeft--;  
-        if (countdownEl.textContent = timeLeft
-        // timeLeft--10);
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
 
-        if (timeLeft < 0) {
-            // timer stops and user is taken to the highscores page
-        }
-    })
+// Need to figure out how to get timer button working and how to deduct -10 if answer is wrong
 
-    render()
-})
+
+// else (timeLeft < 0) {
+// timer stops and user is taken to the highscores page
+// }
+
+
+// })
 
 function render() {
     let questionData = quizQuestions[currentQuestion];
     $("#question").text(questionData.question);
     $("#choices").empty();
-
+    // should i include <li>? i took the <li> out of my buttons and am using just <button> for the answers now
     for (let i = 0; i < questionData.choices; i++) {
-        $("#options").append($("<li>").text(questionData.choices[i]));
+        $("#choices").append($("<li>").text(questionData.choices[i]));
     }
-    currentQuestion +- 1;
-    if (currentQuestion === quizQuestions.length) {
+    currentQuestion + -1;
+    if (currentQuestion === questionData.length) {
         currentQuestion = 0;
     }
 }
 
-$("#submit").click(render)
+// $("#submit").click(render)
 
 
-
-//Questions for loop
-// for (i = 0; i < quizQuestions.length; i++ ) {
-//     let response = 
-// }
-// add function to start quiz here
-
-// THEN a timer starts and I am presented with a question
-
-
-//Questions Start
-
-
-
-//WHEN I answer a question
-
-
-//THEN I am presented with another question
-
-
-//WHEN I answer a question incorrectly
-
-
-//THEN time is subtracted from the clock
-
-
-//WHEN all questions are answered or the timer reaches 0
-
-
-//THEN the game is over
 
 
 //THEN I can save my initials and score - Start of Highscore List to add to
 
 
-renderHighscores();
+
 
 function renderHighscores() {
     // Clear highscoreList element and update highscoreCountSpan
@@ -163,7 +158,7 @@ function renderHighscores() {
 }
 
 // //When the name is submitted to the list
-highscoreForm.addEventListener("submit", function(event) {
+highscoreForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     let highscoreText = highscoreInput.value.trim();
